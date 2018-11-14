ce9bfc8c5942eae167aebcb928bde6d12e27d53a
Amine HARIRI <amine.hariri@corp.talan.com>
2018-11-14 10:10:35 +0100

    first commit

---
+++ src/main/java/com/tennis/kata/TennisSetKataApplication.java
@@ -0,0 +1,12 @@
+package com.tennis.kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisSetKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TennisSetKataApplication.class, args);
+    }
+}
